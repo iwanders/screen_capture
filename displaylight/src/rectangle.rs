ec08aff27740c55dba341fa0d320565ad660c4be
Ivor Wanders <ivor@iwanders.net>
2024-04-26 18:44:24 -0400

    Remove everything unrelated to the screen_capture crate.

--- displaylight/src/rectangle.rs
+++
@@ -1,8 +0,0 @@
-/// Represents a rectangle on a grid.
-#[derive(Debug, Default, Eq, PartialEq, Copy, Clone)]
-pub struct Rectangle {
-    pub x_min: u32,
-    pub x_max: u32,
-    pub y_min: u32,
-    pub y_max: u32,
-}
